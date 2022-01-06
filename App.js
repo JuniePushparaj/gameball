bc28049ea1845cb49f37979f21df32b48e0e7b3a
JuniePushparaj <erjuniebgt@gmail.com>
2022-01-06 22:44:19 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,77 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  Button,
+  StyleSheet,
+  Text,
+  View,
+  ActivityIndicator,
+  ImageBackground
+} from "react-native";
+import { useState } from "react";
+import Coin from "./Coin";
+import image from "../images/gameball.jpeg";
 
-export default function App() {
+function App() {
+  let [side, flip] = useState(0);
+  let [spin, setSpin] = useState(false);
+  let [text, setText] = useState("");
+  const handleClick = () => {
+    setSpin(true);
+    setText("");
+    setTimeout(() => {
+      setSpin(false);
+      let toss = Math.floor(Math.random() * 2);
+      flip(toss);
+      setText(toss ? "Heads" : "Tails");
+    }, 2000);
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.app}>
+      <Text style={styles.title}>Toss:{text}</Text>
+      <ImageBackground source={image} resizeMode="cover" style={styles.image}>
+        <View style={styles.header}>
+          {spin ? <ActivityIndicator size="large" color="#00ff00" /> : ""}
+          {!spin ? <Coin side={side} /> : ""}
+        </View>
+      </ImageBackground>
+      <Button
+        onPress={() => {
+          handleClick();
+        }}
+        title="Flip Coin"
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  app: {
+    flex: 1
   },
+  header: {
+    padding: 20
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: "1.5rem",
+    textAlign: "center"
+  },
+  image: {
+    flex: 1,
+    justifyContent: "center"
+  },
+  text: {
+    lineHeight: "1.5em",
+    fontSize: "1.125rem",
+    marginVertical: "1em",
+    textAlign: "center"
+  },
+  link: {
+    color: "#1B95E0"
+  },
+  code: {
+    fontFamily: "monospace, monospace"
+  }
 });
+
+export default App;
